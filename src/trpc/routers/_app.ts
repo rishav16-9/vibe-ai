28870238f8527881538eb7237f4e3763a91f0f7b
Rishav <suranarishav08@gmail.com>
2025-07-04 16:14:17 +0530

    Messages

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,32 +1,7 @@
-import { z } from "zod";
-import { inngest } from "@/inngest/client";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { messageRouter } from "@/module/messages/server/procedures";
+import { createTRPCRouter } from "../init";
 export const appRouter = createTRPCRouter({
-  invoke: baseProcedure
-    .input(
-      z.object({
-        value: z.string(),
-      })
-    )
-    .mutation(async ({ input }) => {
-      await inngest.send({
-        name: "test/hello.world",
-        data: {
-          value: input.value,
-        },
-      });
-    }),
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  messages: messageRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
